4036b7e8e5e27a8b7440e75b97d34dc85a0ce1d6
Barsik <magnit_322@rambler.ru>
2023-03-28 14:48:53 +0400

    Splash Screen, resources

---
+++ app/src/main/java/ru/barsik/wanttohelp/SplashActivity.kt
@@ -0,0 +1,30 @@
+package ru.barsik.wanttohelp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.widget.LinearLayout
+import android.widget.TextView
+import ru.barsik.wanttohelp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            },
+            2000
+        )
+    }
+}
