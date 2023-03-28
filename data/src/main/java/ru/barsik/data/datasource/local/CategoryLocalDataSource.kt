3e37a5d4a54b7b5a7c8949efe9721db4c5d916f5
Barsik <magnit_322@rambler.ru>
2023-03-28 14:47:25 +0400

    Category DataSource, Repository

--- data/src/main/java/ru/barsik/data/datasource/local/CategoryLocalDataSource.kt
+++ data/src/main/java/ru/barsik/data/datasource/local/CategoryLocalDataSource.kt
@@ -1,9 +1,24 @@
 package ru.barsik.data.datasource.local
 
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.barsik.data.datasource.CategoryDataSource
+import ru.barsik.domain.model.Category
+
 /**
  *
  * @author Mitryashkin
  * @since 28.03.2023
  */
-class CategoryLocalDataSource {
+class CategoryLocalDataSource(private val ctx: Context) : CategoryDataSource {
+
+    private val _fileName = "categories.json"
+    override suspend fun getCategories(): List<Category> {
+        val jsonString = ctx.assets.open(_fileName)
+            .bufferedReader().use { it.readText() }
+        val gson = Gson()
+        val catKeyType = object : TypeToken<List<Category>>() {}.type
+        return gson.fromJson(jsonString, catKeyType)
+    }
 }
--- data/src/main/java/ru/barsik/data/repository/CategoryRepositoryImpl.kt
+++ data/src/main/java/ru/barsik/data/repository/CategoryRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package ru.barsik.data.repository
 
+import ru.barsik.data.datasource.local.CategoryLocalDataSource
+import ru.barsik.data.datasource.remote.CategoryRemoteDataSource
 import ru.barsik.domain.model.Category
 import ru.barsik.domain.repository.CategoryRepository
 
@@ -8,8 +10,11 @@ import ru.barsik.domain.repository.CategoryRepository
  * @author Mitryashkin
  * @since 28.03.2023
  */
-class CategoryRepositoryImpl : CategoryRepository {
+class CategoryRepositoryImpl(
+    private val localDS: CategoryLocalDataSource,
+    private val remoteDS: CategoryRemoteDataSource
+) : CategoryRepository {
     override suspend fun getAllCategories(): List<Category> {
-        TODO("Not yet implemented")
+        return localDS.getCategories()
     }
 }
