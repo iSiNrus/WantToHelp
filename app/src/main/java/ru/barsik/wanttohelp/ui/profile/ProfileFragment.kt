9d518002a9ccd52c01143fe23b7de6ba1bc1360d
iSiNrus <magnit_322@rambler.ru>
2023-04-03 23:05:31 +0400

    Профиль

---
+++ app/src/main/java/ru/barsik/wanttohelp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package ru.barsik.wanttohelp.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.barsik.wanttohelp.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
