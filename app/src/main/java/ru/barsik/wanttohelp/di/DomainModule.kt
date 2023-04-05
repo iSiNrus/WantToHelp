2d2a0689839a3c7131ae51cc19786ae36c1ded15
iSiNrus <magnit_322@rambler.ru>
2023-04-05 22:23:51 +0400

    DomainModule

--- app/src/main/java/ru/barsik/wanttohelp/di/DomainModule.kt
+++ app/src/main/java/ru/barsik/wanttohelp/di/DomainModule.kt
@@ -1,7 +1,41 @@
 package ru.barsik.wanttohelp.di
 
 import dagger.Module
+import dagger.Provides
+import ru.barsik.domain.repository.CategoryRepository
+import ru.barsik.domain.repository.EventRepository
+import ru.barsik.domain.usecase.GetAllEventsUseCase
+import ru.barsik.domain.usecase.GetEventByIdUseCase
+import ru.barsik.domain.usecase.SearchEventByNKOUseCase
+import ru.barsik.domain.usecase.SearchEventByTitleUseCase
+import ru.barsik.domain.usecase.categories.GetAllCategoriesUseCase
 
 @Module
 class DomainModule {
+
+    @Provides
+    fun provideGetAllCategoriesUseCase(categoryRepository: CategoryRepository): GetAllCategoriesUseCase {
+        return GetAllCategoriesUseCase(categoryRepository)
+    }
+
+    @Provides
+    fun provideGetEventByIdUseCase(eventRepository: EventRepository): GetEventByIdUseCase {
+        return GetEventByIdUseCase(eventRepository)
+    }
+
+    @Provides
+    fun providesGetAllEventsUseCase(eventRepository: EventRepository): GetAllEventsUseCase {
+        return GetAllEventsUseCase(eventRepository)
+    }
+
+    @Provides
+    fun provideSearchEventByTitleUseCase(eventRepository: EventRepository): SearchEventByTitleUseCase {
+        return SearchEventByTitleUseCase(eventRepository)
+    }
+
+    @Provides
+    fun provideSearchEventByNKOUseCase(eventRepository: EventRepository): SearchEventByNKOUseCase {
+        return SearchEventByNKOUseCase(eventRepository)
+    }
+
 }
