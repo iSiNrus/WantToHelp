afefee54d5b9ffe440d47e6d6b995aab6035f522
BarsikMSI <magnit_322@rambler.ru>
2023-04-20 15:17:30 +0400

    ViewModelScope правильные аннотации

--- app/src/main/java/ru/barsik/wanttohelp/di/ViewModelScope.kt
+++ app/src/main/java/ru/barsik/wanttohelp/di/ViewModelScope.kt
@@ -1,9 +1,7 @@
 package ru.barsik.wanttohelp.di
 
-import java.lang.annotation.RetentionPolicy
-import java.lang.annotation.Retention
 import javax.inject.Scope
 
 @Scope
-@Retention(RetentionPolicy.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ViewModelScope()
